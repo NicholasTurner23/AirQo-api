5f812afa72c65fdbc4d2ed8b44a8550329864849
NicholasTurner23 <mugtrade@gmail.com>
2025-03-27 13:18:05 +0300

    Update doc strings

--- src/workflows/airqo_etl_utils/datautils.py
+++ src/workflows/airqo_etl_utils/datautils.py
@@ -355,6 +355,7 @@ class DataUtils:
             device_network(DeviceNetwork, optional): The network to filter devices, default is None (no filter).
             dynamic_query(bool, optional): Determines the type of data returned. If True, returns averaged data grouped by `device_number`, `device_id`, and `site_id`. If False, returns raw data without aggregation. Defaults to False.
             remove_outliers(bool, optional): If True, removes outliers from the extracted data. Defaults to True.
+            data_filter(dict, optional): A dictionary used to apply a `WHERE` clause when building the query. The format should be `{'field': value}`. The value can be a single literal or a list of values for filtering multiple entries.
 
         Returns:
             pd.DataFrame: A pandas DataFrame containing the cleaned data from BigQuery.
